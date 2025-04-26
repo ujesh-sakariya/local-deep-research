aa363d8dd4d307b95ad81e033a31a390e327f69a
LearningCircuit <185559241+LearningCircuit@users.noreply.github.com>
2025-04-26 12:17:00 +0200

    logger info to debug

--- src/local_deep_research/advanced_search_system/strategies/parallel_search_strategy.py
+++ src/local_deep_research/advanced_search_system/strategies/parallel_search_strategy.py
@@ -118,7 +118,7 @@ class ParallelSearchStrategy(BaseSearchStrategy):
 
         # Determine number of iterations to run
         iterations_to_run = get_db_setting("search.iterations")
-        logger.info("Selected amount of iterations: " + iterations_to_run)
+        logger.debug("Selected amount of iterations: " + iterations_to_run)
         iterations_to_run = int(iterations_to_run)
         try:
             # Run each iteration
