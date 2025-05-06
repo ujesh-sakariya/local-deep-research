dab134f8806100694d6d4f1d19204e0c66c198a0
LearningCircuit <185559241+LearningCircuit@users.noreply.github.com>
2025-05-06 22:16:37 +0200

    fixed a minor bug for programmatic acces, some functions are not available - I think

--- src/local_deep_research/api/__init__.py
+++ src/local_deep_research/api/__init__.py
@@ -6,8 +6,6 @@ API module for programmatic access to Local Deep Research functionality.
 from .research_functions import (
     analyze_documents,
     generate_report,
-    get_available_collections,
-    get_available_search_engines,
     quick_summary,
 )
 
@@ -15,6 +13,4 @@ __all__ = [
     "quick_summary",
     "generate_report",
     "analyze_documents",
-    "get_available_search_engines",
-    "get_available_collections",
 ]
